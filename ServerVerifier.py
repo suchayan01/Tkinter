69c45632166c2ed10aa0c81f430a0fa7fdccfc65
suchayan01 <suchayanbiswas@gmail.com>
2023-05-03 09:18:29 +0530

    Grid

--- ServerVerifier.py
+++ ServerVerifier.py
@@ -1,8 +1,7 @@
 from tkinter import *
 root= Tk()
-myLabel1= Label(root, text ="Hello World")
-myLabel2 = Label(root, text ="Something giberish")
-myLabel1.grid(row=0,column=0)
-myLabel2.grid(row=0,column=1)
+myLabel1= Label(root, text ="Hello World").grid(row=0,column=0)
+myLabel2 = Label(root, text ="Something giberish").grid(row=0,column=1)
+
 
 root.mainloop()
