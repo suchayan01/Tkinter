f594c6a10e417d21e35f1078133ed17e84fc8762
suchayan01 <suchayanbiswas@gmail.com>
2023-05-03 09:29:45 +0530

    Button with bg and fg  & func call

--- Buttons.py
+++ Buttons.py
@@ -1,5 +1,12 @@
 from tkinter import *
 root= Tk()
-myButton= Button(root, text= "Click me",state=DISABLED)
+
+def myClick():
+    myLabel = Label(root, text="Click happened")
+    myLabel.pack()
+# myButton= Button(root, text= "Click me",state=DISABLED,padx=10,pady=10)
+myButton= Button(root, text= "Click me",command=myClick,fg="blue",bg="white")
 myButton.pack()
+
+
 root.mainloop()
